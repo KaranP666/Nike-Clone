82125befbfbd526b57a1253f6592105896fb21ec
KARAN PANCHAL <83541879+KaranP666@users.noreply.github.com>
2023-12-30 20:43:16 +0530

    Navbar and Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-// import './App.css'
+import React from "react";
+import {
+  CustomerReviews,
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  Footer,
+} from "./section";
+import Nav from "./components/Nav";
+
 const App = () => {
   return (
-    <main className='relative'>
-      Nav 
-      <section className='xl:padding-1 wide:padding-r padding-b'>
-        Hero
-        <section className='padding'>
-          PopularProducts  
-        </section>
-        <section className='padding'>
-          SuperQuality  
-        </section>
-        <section className='padding-x py-10'>
-          Services  
-        </section>
-        <section className='padding'>
-          SpecialOffer  
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+        <section className="padding"><PopularProducts /></section>
+        <section className="padding"><SuperQuality /></section>
+        <section className="padding-x py-10"><Services /></section>
+        <section className="padding"><SpecialOffer /></section>
+        <section className="bg-pale-blue padding"><CustomerReviews /></section>
+        <section className="padding-x sm:py-32 w-full"><Subscribe /></section>
+        <section className="padding bg-black padding-x padding-t pb-8">
+          Footer
         </section>
       </section>
-
     </main>
-    
-  )
-}
+  );
+};
 
 export default App;
